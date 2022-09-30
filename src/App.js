1f36ee1833c5af4a1aad18b2847f827dec6ef8cb
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-30 17:37:56 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>Connect</h1>
+      <Header />
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
-import React from 'react'
-
-const Header = () => {
-  return <div>Header</div>
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  )
+}
+Header.defaultProps = {
+  title: 'Task Master'
 }
 
 export default Header
