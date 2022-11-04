f7943a339e9bea6b5968f47556047aa78c59a7cf
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-11-04 18:11:12 -0500

    beginning to understand material ui formgroup

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,24 +1,25 @@
-import { Checkbox } from '@material-ui/core'
-import FormGroup from '@material-ui/core'
+import {
+  Checkbox,
+  FormControlLabel,
+  TextField,
+  FormGroup,
+  Typography
+} from '@material-ui/core'
+
 const AddTask = () => {
   return (
-    <form className="add-form">
-      <div className="form-control">
-        <label> Task</label>
-        <input type="text" placeholder=" Add Task"></input>
-      </div>
-
+    <FormGroup>
+      <TextField label={'Add Task'} />
       <div className="form-control">
         <label> Day and Time</label>
         <input type="text" placeholder="Add Day and Time"></input>
       </div>
-
       <div className="form-control">
         <label> Set Reminder</label>
 
         <Checkbox />
       </div>
-    </form>
+    </FormGroup>
   )
 }
 
