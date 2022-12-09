87f26d440943a86f1145d257b79335925f1bb5db
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-12-08 18:08:57 -0600

    working to add edit function

--- src/components/Task.js
+++ src/components/Task.js
@@ -51,7 +51,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-const Task = ({ task, onDelete, onToggle }) => {
+const Task = ({ task, onDelete, onToggle, editTask }) => {
   const classes = useStyles()
   return (
     <Grid
@@ -82,6 +82,7 @@ const Task = ({ task, onDelete, onToggle }) => {
               <Button
                 className={classes.clearButton}
                 onClick={() => onToggle(task.id)}
+                onClick={editTask}
               >
                 <img
                   src="https://img.icons8.com/ios/512/reminder.png"
